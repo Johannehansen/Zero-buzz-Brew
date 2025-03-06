f796ab78bcc4ffc0bfaca98b1a2ea7f2bc889d9d
Julieloft <eaa24jljen@students.eaaa.dk>
2025-03-06 11:10:23 +0100

    meta tags og styling af quiz

--- js.js
+++ js.js
@@ -6,14 +6,14 @@ function nextQuestion(answer) {
     answers.push(answer);
 
     if (step === 1) { // første spørgsmål vises
-        document.getElementById('question').innerText = 'Foretrækker du Pilsner, IPA eller Classic?'; // afventer input fra bruger
+        document.getElementById('question').innerText = 'Hvilken slags øl foretrækker du normalt?'; // afventer input fra bruger
         document.getElementById('options').innerHTML = ` 
             <div class="option" onclick="nextQuestion('Pilsner')">Pilsner</div>
             <div class="option" onclick="nextQuestion('IPA')">IPA</div>
             <div class="option" onclick="nextQuestion('Classic')">Classic</div> 
         `; // ved onclick skifter spørgsmål i realtid
     } else if (step === 2) { 
-        document.getElementById('question').innerText = 'Kan du lide din øl fyldig eller bitter?'; // afventer input fra bruger
+        document.getElementById('question').innerText = 'Hvordan kan du bedst lide din øl?'; // afventer input fra bruger
         document.getElementById('options').innerHTML = `
             <div class="option" onclick="nextQuestion('Fyldig')">Fyldig</div> 
             <div class="option" onclick="nextQuestion('Bitter')">Bitter</div>
@@ -25,7 +25,7 @@ function nextQuestion(answer) {
         // nedenfor er der implementeret en src-attribut for at gøre quizzen mere visuel
         let imgSrc = '';
         if (answers[1] === 'Pilsner') imgSrc = '/images/øl1.png';
-        else if (answers[1] === 'IPA') imgSrc = '/images/øl2.png';
+        else if (answers[1] === 'IPA') imgSrc = '/images/øl6.png';
         else if (answers[1] === 'Classic') imgSrc = '/images/øl3.png';
 
         document.getElementById('resultImg').src = imgSrc;
